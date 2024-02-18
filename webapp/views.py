1e08f21494a7297019c5c44380b7017d99f63f35
sarboleda72 <sarboleda72@gmail.com>
2024-02-18 16:32:38 -0500

    Se hace la lectura con pandas, se instalan dependencias

--- webapp/views.py
+++ webapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 import pandas as pd
+import os
 
 
 # Create your views here.
@@ -12,8 +13,16 @@ def index(request):
     if request.method == 'POST' and request.FILES['excel']:
         excel = request.FILES['excel']
         fs =FileSystemStorage()
-        filename= fs.save(excel.name, excel)  # Guarda el archivo en la carpeta de media
+        if fs.exists('db.xlsx'):
+            fs.delete('db.xlsx')
+        filename= fs.save('db.xlsx', excel)  # Guarda el archivo en la carpeta de media
         uploaded_file_url=fs.url(filename)   # Genera una URL para acceder al archivo guardado
+        
+        ruta_archivo = os.path.join(settings.MEDIA_ROOT, filename)
+        #captura pandas
+        df = pd.read_excel(ruta_archivo)
+        
+        
         return render(request, 'index.html', {'upload_file_url':uploaded_file_url})
     
     mensaje=calculoMetricas()
