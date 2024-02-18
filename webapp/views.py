a757560647bfef9d1a84c5583fe12015d6412b4f
sarboleda72 <sarboleda72@gmail.com>
2024-02-18 15:46:47 -0500

    Se sube archivo y queda guardado temporalmente

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,20 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+import pandas as pd
 
 
 # Create your views here.
+    
+def calculoMetricas():
+    return {'decercion':'50%'}
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST' and request.FILES['excel']:
+        excel = request.FILES['excel']
+        fs =FileSystemStorage()
+        filename= fs.save(excel.name, excel)  # Guarda el archivo en la carpeta de media
+        uploaded_file_url=fs.url(filename)   # Genera una URL para acceder al archivo guardado
+        return render(request, 'index.html', {'upload_file_url':uploaded_file_url})
+    
+    mensaje=calculoMetricas()
+    return render(request, 'index.html', mensaje)
