a4650bb1716a6eb1a6d5288165150fe0d7d22c17
sarboleda72 <sarboleda72@gmail.com>
2024-02-19 13:06:09 -0500

    Se ha solucionado bug de cambios de graficos

--- webapp/static/assets/js/dashboard.js
+++ webapp/static/assets/js/dashboard.js
@@ -1,109 +1,202 @@
+
 $(function () {
 
 
   // =====================================
   // Profit
   // =====================================
-  var chart = {
-    series: [
-      { name: "Earnings this month:", data: [355, 390, 300, 350, 390, 180, 355, 390] },
-      { name: "Expense this month:", data: [280, 250, 325, 215, 250, 310, 280, 250] },
-    ],
 
-    chart: {
-      type: "bar",
-      height: 345,
-      offsetX: -15,
-      toolbar: { show: true },
-      foreColor: "#adb0bb",
-      fontFamily: 'inherit',
-      sparkline: { enabled: false },
-    },
+  var select = document.getElementById('razones');
+  var chartDiv=document.getElementById('chart');
+
+  // Agregar un listener para el evento de cambio
+  select.addEventListener('change', function () {
+    // Obtener el valor seleccionado
+    var valorSeleccionado = select.value;
+    
+    if (valorSeleccionado == 1) {
+      chartDiv.innerHTML="";
+      
+      graficoBarras('factor', [["Dificultades","económicas"], ["Problemas personales/","familiares"], ["Falta de interés en el"," programa de formación"], ["Falta de apoyo","académico"], ["Oportunidades laborales","externas"]]);
+    }
+    if (valorSeleccionado == 2) {
+      chartDiv.innerHTML="";
+      graficoBarras('edad', ["Mayor de edad", "Menor de edad"]);
+    }
+    if(valorSeleccionado==3){
+      chartDiv.innerHTML="";
+      graficoBarrasColores();
+    }
+    if(valorSeleccionado==4){
+      chartDiv.innerHTML="";
+      chartDiv.removeAttribute("style");
+    }
+  });
+
+  // Llamar a la función para cargar el gráfico al inicio
+  graficoBarras('factor', [["Dificultades","económicas"], ["Problemas personales/","familiares"], ["Falta de interés en el"," programa de formación"], ["Falta de apoyo","académico"], ["Oportunidades laborales","externas"]]);
 
 
-    colors: ["#5D87FF", "#49BEFF"],
+  function graficoBarras(nombreOpcion, categorias) {
+    var vector = JSON.parse(document.getElementById(nombreOpcion).getAttribute('data-vector'));
 
+    var chart = {
+      series: [
+        { name: "", data: vector },
+        //{ name: "Expense this month:", data: [280, 250, 325, 215, 250, 310, 280, 250] },
+      ],
 
-    plotOptions: {
-      bar: {
-        horizontal: false,
-        columnWidth: "35%",
-        borderRadius: [6],
-        borderRadiusApplication: 'end',
-        borderRadiusWhenStacked: 'all'
+      chart: {
+        type: "bar",
+        height: 345,
+        offsetX: -15,
+        toolbar: { show: true },
+        foreColor: "#adb0bb",
+        fontFamily: 'inherit',
+        sparkline: { enabled: false },
       },
-    },
-    markers: { size: 0 },
-
-    dataLabels: {
-      enabled: false,
-    },
 
 
-    legend: {
-      show: false,
-    },
+      colors: ["#5D87FF", "#49BEFF"],
 
 
-    grid: {
-      borderColor: "rgba(0,0,0,0.1)",
-      strokeDashArray: 3,
-      xaxis: {
-        lines: {
-          show: false,
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: "35%",
+          borderRadius: [6],
+          borderRadiusApplication: 'end',
+          borderRadiusWhenStacked: 'all'
         },
       },
-    },
+      markers: { size: 0 },
 
-    xaxis: {
-      type: "category",
-      categories: ["16/08", "17/08", "18/08", "19/08", "20/08", "21/08", "22/08", "23/08"],
-      labels: {
-        style: { cssClass: "grey--text lighten-2--text fill-color" },
+      dataLabels: {
+        enabled: false,
       },
-    },
 
 
-    yaxis: {
-      show: true,
-      min: 0,
-      max: 400,
-      tickAmount: 4,
-      labels: {
-        style: {
-          cssClass: "grey--text lighten-2--text fill-color",
-        },
+      legend: {
+        show: false,
       },
-    },
-    stroke: {
-      show: true,
-      width: 3,
-      lineCap: "butt",
-      colors: ["transparent"],
-    },
 
 
-    tooltip: { theme: "light" },
-
-    responsive: [
-      {
-        breakpoint: 600,
-        options: {
-          plotOptions: {
-            bar: {
-              borderRadius: 3,
-            }
+      grid: {
+        borderColor: "rgba(0,0,0,0.1)",
+        strokeDashArray: 3,
+        xaxis: {
+          lines: {
+            show: false,
           },
+        },
+      },
+
+      xaxis: {
+        type: "category",
+        categories: categorias,
+        labels: {
+          style: { cssClass: "grey--text lighten-2--text fill-color" },
+        },
+      },
+
+
+      yaxis: {
+        show: true,
+        min: 0,
+        max: Math.ceil(Math.max(...vector) / 10) * 10,
+        tickAmount: 4,
+        labels: {
+          style: {
+            cssClass: "grey--text lighten-2--text fill-color",
+          },
+        },
+      },
+      stroke: {
+        show: true,
+        width: 3,
+        lineCap: "butt",
+        colors: ["transparent"],
+      },
+
+
+      tooltip: { theme: "light" },
+
+      responsive: [
+        {
+          breakpoint: 600,
+          options: {
+            plotOptions: {
+              bar: {
+                borderRadius: 3,
+              }
+            },
+          }
+        }
+      ]
+
+
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), chart);
+    chart.render();
+  }
+
+  //=======================================
+  //Grafico barras de colores
+  //======================================
+
+  function graficoBarrasColores(){
+    var colors = ['#FF5733', '#33FF57', '#3357FF', '#FF33F6', '#F6FF33', '#33F6FF', '#A633FF', '#FF33A6'];
+
+    var options = {
+      series: [{
+      data: [21, 22, 10, 28, 16, 21, 13, 30]
+    }],
+      chart: {
+      height: 345,
+      type: 'bar',
+      events: {
+        click: function(chart, w, e) {
+          // console.log(chart, w, e)
         }
       }
-    ]
+    },
+    colors: colors,
+    plotOptions: {
+      bar: {
+        columnWidth: '45%',
+        distributed: true,
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    legend: {
+      show: false
+    },
+    xaxis: {
+      categories: [
+        ['John', 'Doe'],
+        ['Joe', 'Smith'],
+        ['Jake', 'Williams'],
+        'Amber',
+        ['Peter', 'Brown'],
+        ['Mary', 'Evans'],
+        ['David', 'Wilson'],
+        ['Lily', 'Roberts'], 
+      ],
+      labels: {
+        style: {
+          colors: colors,
+          fontSize: '12px'
+        }
+      }
+    }
+    };
 
-
-  };
-
-  var chart = new ApexCharts(document.querySelector("#chart"), chart);
-  chart.render();
-
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+  }
 
   // =====================================
   // Breakup
@@ -156,8 +249,8 @@ $(function () {
     },
   };
 
-  var chart = new ApexCharts(document.querySelector("#breakup"), breakup);
-  chart.render();
+  var breakup = new ApexCharts(document.querySelector("#breakup"), breakup);
+  breakup.render();
 
 
 
--- webapp/views.py
+++ webapp/views.py
@@ -19,6 +19,39 @@ def calculoDesercion(estadoAprendiz):
     porcentajeDesercion= str(round(porcentajeDesercion,2))+'%'
     return {'desertados':porcentajeDesercion,'total':len(estadoAprendiz),'cancelado':cancelado,'retirado':retiroVoluntario}
 
+def calculoFactor(factorAprendiz,estadoAprendiz):
+    problemasPersonales=0
+    dificultadesEconomicas=0
+    faltaDeInteres=0
+    faltaApoyoEconomico=0
+    oportunidadesLaborales=0
+    
+    for factor,estado in zip(factorAprendiz,estadoAprendiz):
+        if estado=='Cancelado' or estado=='Retiro voluntario':
+            if factor== 'Problemas personales / familiares':
+                problemasPersonales+=1
+            elif factor == 'Dificultades económicas' :
+                dificultadesEconomicas += 1
+            elif factor == 'Falta de interés en el programa de formación':
+                faltaDeInteres += 1
+            elif factor == 'Falta de apoyo académico':
+                faltaApoyoEconomico += 1
+            elif factor == 'Oportunidades laborales externas':
+                oportunidadesLaborales+=1
+    
+    return [dificultadesEconomicas,problemasPersonales,faltaDeInteres,faltaApoyoEconomico,oportunidadesLaborales]
+
+def calculoEdad(tipoDocumento,estadoAprendiz):
+    mayorEdad=0
+    menorEdad=0
+    for tipoDocumento,estado in zip(tipoDocumento,estadoAprendiz):
+        if estado=='Cancelado' or estado=='Retiro voluntario':
+            if tipoDocumento== 'CC' or tipoDocumento== 'CE' or tipoDocumento== 'PPT' :
+                mayorEdad+=1
+            elif tipoDocumento == 'TI':
+                menorEdad += 1
+    return [mayorEdad,menorEdad]
+
 def index(request):
     if request.method == 'POST' and request.FILES['excel']:
         excel = request.FILES['excel']
@@ -28,12 +61,19 @@ def index(request):
         filename= fs.save('db.xlsx', excel)  # Guarda el archivo en la carpeta de media
         uploaded_file_url=fs.url(filename)   # Genera una URL para acceder al archivo guardado
         ruta_archivo = os.path.join(settings.MEDIA_ROOT, filename)#captura ruta interna
+        
         #captura pandas
         df = pd.read_excel(ruta_archivo)
+        
+        #Llamado de funciones que calculan las metricas
         desercion=calculoDesercion(df['ESTADO_APRENDIZ'])
+        factor=calculoFactor(df['FACTORES'],df['ESTADO_APRENDIZ'])
+        edad=calculoEdad(df['TIPO_DOCUMENTO'],df['ESTADO_APRENDIZ'])
         
         mensaje={
-            'desercion':desercion
+            'desercion':desercion,
+            'factor':factor,
+            'edad':edad
         }
         return render(request, 'index.html',mensaje)
     
